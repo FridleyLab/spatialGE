835f5ba2d1cf3711e0b5ca36c501642b02bcb7b3
oospina <oscaredd_ol@yahoo.es>
2025-03-03 21:52:39 -0500

    fix data folder

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@ thrane_files = function() {
   #ns = topenv()
   # Get the full path to  data file within the package
   #ns$thrane_files = system.file("inst/extdata/melanoma_thrane", package='spatialGE')
-  thrane_files = system.file("inst/extdata/melanoma_thrane", package='spatialGE')
+  thrane_files = system.file("extdata/melanoma_thrane", package='spatialGE')
   return(thrane_files)
   # Load the data using the path
   #data(file=thrane_files, envir=environment())
